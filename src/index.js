7c62f4b8d9fd1b682e0fa57c501b75be7166aae3
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-14 14:56:39 -0600

    Imported createStore and Provider into Index.js and set them up there. Now working on using connect to top into the state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+import { carReducer } from "./reducers";
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+/*
+STEP 1 - Create a store! Pass our store object into the Redux Provider component that is wrapped around <App />
+    a. Give your Provider the store property and equal it to the Value of store.
+     
+    b. Import reducer function from reducer folder
+*/
+
+export const store = createStore(carReducer)
+
+console.log(store)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, rootElement);
